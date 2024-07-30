d585902baba34d9a2d59ebbbab1cdd023c02f79d
irka775 <barbascumpa54@gmail.com>
2024-07-30 21:48:29 +0100

    Update debug var on page for make posible debuging on herocu with environment var

--- juice_app/views.py
+++ juice_app/views.py
@@ -7,6 +7,7 @@ from .forms import CommentForm, RecipeForm
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models import Q
+from juice_project.settings import DEBUG
 
 
 def ajax_search(request):
@@ -159,7 +160,7 @@ def recipe_list(request):
     return render(
         request,
         "juice_app/recipe_list.html",
-        {"page_obj": page_obj, "recipe_list": recipes, "categories": categories},
+        {"page_obj": page_obj, "recipe_list": recipes, "categories": categories,"DEBUG":DEBUG},
     )
 
 
