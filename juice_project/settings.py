5ef4ac0b08a99f8ddaa901eeb1ec59c6c3fa7582
irka775 <barbascumpa54@gmail.com>
2024-08-01 11:51:02 +0100

    Update slug an DEBUG

--- juice_project/settings.py
+++ juice_project/settings.py
@@ -20,7 +20,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEVELOPMENT")
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
