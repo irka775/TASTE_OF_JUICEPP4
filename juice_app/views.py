36434d99d543b8e633659c379df52a84e112e6b5
irka775 <barbascumpa54@gmail.com>
2024-08-01 12:27:33 +0100

    Update DEBUG VAR (Django not allowed probably use the DEBUG VAr in other files

--- juice_app/views.py
+++ juice_app/views.py
@@ -7,7 +7,6 @@ from .forms import CommentForm, RecipeForm
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models import Q
-from juice_project.settings import DEBUG
 
 
 def ajax_search(request):
@@ -165,7 +164,6 @@ def recipe_list(request):
             "page_obj": page_obj,
             "recipe_list": recipes,
             "categories": categories,
-            "DEBUG": DEBUG,
         },
     )
 
